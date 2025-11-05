2e27a813ff033326986c52e4b765e95464f44add
Jay Marcyes <jay@marcyes.com>
2025-11-04 17:58:41 -0700

    Adds a typing module that pulls in all the different types into one place to make it easier for me to remember where to get specific types. I'm not sure I'll ever actually use this module except as a reference.

---
+++ datatypes/typing.py
@@ -0,0 +1,122 @@
+"""
+Typing hints in one place, I keep having to remember where certain annotations
+are located to import them, hopefully I'll be able to use this as a source
+of truth
+
+https://github.com/python/typeshed - Typeshed contains external type
+annotations for the Python standard library and Python builtins
+
+https://docs.python.org/3/library/typing.html#user-defined-generic-types
+Good overview on how to define custom classes so they can accept annotations
+and use those annotations throughout the class to make sure they expect the
+correct type
+"""
+
+# https://docs.python.org/3/library/typing.html
+from typing import (
+    TypeVar,
+    NewType, # https://docs.python.org/3/library/typing.html#typing.NewType
+    TypeVarTuple, # https://docs.python.org/3/library/typing.html#typing.TypeVarTuple
+
+    T, # https://docs.python.org/3/library/typing.html#generics
+    Generic, # https://docs.python.org/3/library/typing.html#typing.Generic
+    Any,
+    LiteralString,
+    Literal,
+    NoReturn, Never,
+
+    # represents current enclosed class
+    #https://docs.python.org/3/library/typing.html#typing.Self
+    Self,
+    Protocol,
+
+    Union,
+    Optional,
+    ClassVar,
+    Final, # https://docs.python.org/3/library/typing.html#typing.Final
+
+    # Dict with typing info for keys/values
+    # https://docs.python.org/3/library/typing.html#typing.TypedDict
+    TypedDict,
+    Required,
+    NotRequired,
+    ReadOnly,
+
+    # tuple version of TypedDict
+    NamedTuple, # https://docs.python.org/3/library/typing.html#typing.NamedTuple
+
+    Annotated, # https://docs.python.org/3/library/typing.html#typing.Annotated
+    #TypeIs, # https://docs.python.org/3/library/typing.html#typing.TypeIs
+    TypeGuard,
+
+
+    # https://docs.python.org/3/library/typing.html#abcs-and-protocols-for-working-with-i-o
+    IO,
+    TextIO,
+    BinaryIO,
+)
+
+
+# https://docs.python.org/3/library/types.html
+from types import (
+    NoneType,
+    FunctionType,
+    LambdaType,
+    GeneratorType,
+    CoroutineType,
+    AsyncGeneratorType,
+    MethodType,
+    EllipsisType,
+    UnionType,
+    FrameType,
+)
+
+
+# https://docs.python.org/3/library/contextlib.html
+from contextlib import (
+    AbstractContextManager,
+    AbstractAsyncContextManager,
+    ContextDecorator,
+    AsyncContextDecorator,
+)
+
+
+# https://docs.python.org/3/library/typing.html#abcs-and-protocols-for-working-with-i-o
+# from typing import (
+#     IO,
+#     TextIO,
+#     BinaryIO,
+# )
+
+# https://docs.python.org/3/library/io.html
+# from io import (
+#     IOBase,
+#     TextIOBase,
+# )
+
+
+# https://docs.python.org/3/library/collections.abc.html
+from collections.abc import (
+    Container,
+    Hashable,
+    Iterable, # use this for things that just need to be iterated from 0...N
+    Iterator,
+    Reversible,
+    Generator, # https://docs.python.org/3/library/typing.html#annotating-generators-and-coroutines
+    AsyncIterable,
+    AsyncIterator,
+    AsyncGenerator,
+    Sized,
+    Callable, # callbacks, first arg is arguments, second is return type
+    Collection,
+    Sequence,
+    MutableSequence,
+    Set,
+    MutableSet,
+    Mapping, # base class for dict-like behavior
+    MutableMapping,
+    Awaitable,
+    Coroutine,
+    Buffer,
+)
+
